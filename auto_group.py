a11bab65dafd4b193531e7a9159a74955f3e7fbe
Shuhao Cao <scao.math@gmail.com>
2021-01-25 09:12:59 -0600

    Update to a working version

--- auto_group.py
+++ auto_group.py
@@ -1,5 +1,6 @@
 #%%
 import os
+from os.path import join
 import pandas as pd
 import numpy as np
 import csv
@@ -34,55 +35,69 @@ def split_string(string: str, string_lst):
     index_pointer = argsort(index_string)
     return [string_lst[index_lst[i]] for i in index_pointer]
 # %%
-section_roster_files = find_files('txt', '.')
-section_numbers = [s.split('_')[-1].split('.')[0] for s in section_roster_files]
-section_roster_lst = []
-columns_names = ['Semester', 
-                 'Number',  
-                 'Section',
-                 'Name, First or Preferred Name',
-                 'Middle',
-                 'Name, Last',
-                 'Student ID',
-                 'Email Address']
+if __name__ == "__main__": 
+    current_path = os.path.dirname(os.path.abspath(__file__))
 
-course_roster_file = find_files('csv', '.')
-course_df = pd.read_csv(course_roster_file[0])
-# course_df['Name, First or Preferred Name'] = [s.split(',')[-1][1:] for s in course_df['name']]
-# course_df['Name, Last'] = [s.split(',')[0] for s in course_df['name']]
+    section_roster_files = find_files('txt', current_path)
+    course_roster_file = find_files('csv', current_path)
 
-for _file in section_roster_files:
-    with open(_file, newline='') as f:
-        rows = csv.reader(f, delimiter='\t')
-        _roster = []
-        for i, row in enumerate(rows):
-            if row:
-                if i == 0:
-                    columns = split_string(row[0], columns_names)
-                    if 'Middle' in columns:
-                        index_middle = columns.index('Middle')
-                        columns.remove('Middle')
-                    _roster.append(columns)
-                    len_columns = len(columns)
-                else:
-                    row_student = row[0].split()
-                    if len(row_student) == len_columns+1:
-                        _ = row_student.pop(index_middle)
-                    else:
-                        pass
-                    _roster.append(row_student)
-    section_df = pd.DataFrame(_roster)
-    section_df.columns = section_df.iloc[0]
-    section_df = section_df[1:]
+    section_numbers = [s.split('_')[-1].split('.')[0] for s in section_roster_files]
+    section_roster_lst = []
+    columns_names = ['Semester', 
+                    'Number',  
+                    'Section',
+                    'Name, First or Preferred Name',
+                    'Middle',
+                    'Name, Last',
+                    'Student ID',
+                    'Email Address']
 
-    try:
-        section_df['group_name'] = ['Section '+s for s in section_df.Section]
-        section_df['name'] = section_df[['Name, Last', 'Name, First or Preferred Name']].agg(', '.join, axis=1)
-        course_df = pd.merge(course_df, section_df[['name', 'group_name']],  how='left', 
-             left_on=['name'], right_on = ['name'], suffixes=('_x','')).drop(columns=['group_name_x'])
-    except:
-        print('No section found.')
-        pass
     
-    # section_roster_lst.append(_roster)
-# %%
+    course_df = pd.read_csv(course_roster_file[0])
+    # course_df['Name, First or Preferred Name'] = [s.split(',')[-1][1:] for s in course_df['name']]
+    # course_df['Name, Last'] = [s.split(',')[0] for s in course_df['name']]
+    course_df[['group_name']] = None
+
+    for _file in section_roster_files:
+        print(f'Processing {_file}')
+        with open(_file, newline='') as f:
+            rows = csv.reader(f, delimiter='\t')
+            _roster = []
+            for i, row in enumerate(rows):
+                if row:
+                    if i == 0:
+                        columns = split_string(row[0], columns_names)
+                        if 'Middle' in columns:
+                            index_middle = columns.index('Middle')
+                            columns.remove('Middle')
+                        _roster.append(columns)
+                        len_columns = len(columns)
+                    else:
+                        row_student = row[0].split()
+                        try:
+                            if len(row_student) == len_columns + 1:
+                                _ = row_student.pop(index_middle)
+                        except:
+                            pass
+                        _roster.append(row_student)
+        section_df = pd.DataFrame(_roster)
+        section_df.columns = section_df.iloc[0]
+        section_df = section_df[1:]
+        section_df['name'] = section_df[['Name, Last', 'Name, First or Preferred Name']].agg(', '.join, axis=1)
+
+        try:
+            section_df['group_name'] = ['Section '+s for s in section_df.Section]
+            course_df = pd.merge(course_df, section_df[['name', 'group_name']],
+                                how='left', 
+                                left_on=['name'], 
+                                right_on = ['name'], 
+                                suffixes=('','_new'))
+            course_df['group_name'] = course_df['group_name'].fillna(course_df['group_name_new'])
+            course_df = course_df.drop(columns=['group_name_new'])
+            print(f'{_file} merged to course roster. \n')
+        except:
+            print('No section found.\n')
+            pass
+    
+    course_name = course_df.sections.unique()[0].split(' - ')[0]
+    course_df.to_csv(os.path.join(current_path, f'{course_name}_groups.csv'),index=False)   
